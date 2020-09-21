3f52b8bf766b4fcf03570f06ea1a93b5b136ac00
radarm <71643316+radarm@users.noreply.github.com>
2020-09-21 15:01:24 +0200

    Create otszog.py

---
+++ otszog.py
@@ -0,0 +1,15 @@
+import turtle               # allows us to use the turtles library
+wn = turtle.Screen()        # creates a graphics window
+alex = turtle.Turtle()      
+alex. forward(75)
+alex. left(72)
+alex. forward(75)
+alex. left(72)
+alex. forward(75)
+alex. left(72)
+alex. forward(75)
+alex. left(72)
+alex. forward(75)
+alex. left(72)
+alex. left(360)
+
